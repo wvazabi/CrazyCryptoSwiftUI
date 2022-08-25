db99c6140a89a1a2928987dcadf251a2c5c79363
Enes Kaya <eng.eneskaya@gmail.com>
2022-08-25 16:24:04 +0300

    Crypto Crazy SwiftUI project

--- CryptoCrazySwiftUI/Model/CryptoCurrency.swift
+++ CryptoCrazySwiftUI/Model/CryptoCurrency.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct CryptoCurrency : Hashable,Decodable, Identifiable{
+    
+    let id = UUID()
+    let currency : String
+    let price : String
+    
+    private enum CodingKeys : String, CodingKey {
+     
+        case currency = "currency"
+        case price =    "price"
+    }
+    
+    
+}
--- CryptoCrazySwiftUI/View/MainView.swift
+++ CryptoCrazySwiftUI/View/MainView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct MainView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    @ObservedObject var cryptoListViewModel : CryptoListViewModel
+
+    init() {
+          self.cryptoListViewModel = CryptoListViewModel()
     }
+    
+    
+    var body: some View {
+        NavigationView{
+        
+            
+            List(cryptoListViewModel.cryptoList,id:\.id) { crypto in
+          
+                VStack{
+    
+                Text(crypto.currency)
+                        .font(.title3)
+                        .foregroundColor(.red)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                Text(crypto.price)
+                        .foregroundColor(.blue)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                }
+            
+            }.toolbar(content: {
+                Button {
+                    //
+                    Task.init {
+                        //cryptoListViewModel.cryptoList = []
+                        await cryptoListViewModel.downloadCryptosContinuation(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+                    }
+                } label: {
+                    Text("Refresh")
+                }
+
+            })
+            .navigationTitle(Text("Crypto Crazy"))
+            
+        }.task{
+            /*
+            await cryptoListViewModel.downloadCryptosAsync(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+             */
+            
+            await cryptoListViewModel.downloadCryptosContinuation(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+        }
+        
+        /*.onAppear {
+            cryptoListViewModel.downloadCryptos(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+        }*/
+    }
+
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
